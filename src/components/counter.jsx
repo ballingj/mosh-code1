376663c6f2685b7091d86e8583d9dfbb31315c4a
Jeff Ballinger <39890602+ballingj@users.noreply.github.com>
2021-12-01 12:15:06 -0600

    update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,22 +6,32 @@ class Counter extends Component {
     tags: ['tag1', 'tag2', 'tag3'],
   };
 
+  // old way of rebinding 'this' -- solved by arrow function
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  //arrow function prevents 'this' from rebinding to windows obj
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
+    //console.log('Increment Clicked', this.state.count);
+    // obj.method();  //this is referencing obj
+    // function()  // this is referencing windown obj - in strict mode = undefined
+  }
+
   //setting attribute
   style = {
     fontSize: 12,
     fontWeight: 'bold',
   };
 
-  handleIncrement() {
-    console.log('Increment Clicked');
-  }
-
   render() {
     return (
       <Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={this.handleIncrement}  //we do not call the method/function: we are passing a reference
           style={this.style}
           className="btn btn-secondary btn-sm"
         >
