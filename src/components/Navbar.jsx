927588a6c16704e49dfb31ab01cbd46ee56f37b3
Jeff Ballinger <kumite@MacBook-Pro-16.localdomain>
2021-12-10 23:07:27 -0600

    stateless functional component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 
- class NavBar extends Component {
-  render() { 
-    return (
-      // eslint-disable-next-line
-      <div>
-        <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            Navbar{' '}
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </nav>
-      </div>
-    );  
-  }
+ const NavBar = (props) => {
+  
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar{' '}
+          <span className="badge badge-pill badge-secondary">
+            {props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    </div>
+  );  
 }
  
 export default NavBar;
